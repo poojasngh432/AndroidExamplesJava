dd034b175ebf6c84696a8976cfac1cc440a0c84d
Pooja Singh <poojasngh432@gmail.com>
2020-08-29 19:38:22 +0530

    - handling permissions

---
+++ PermissionsExample/app/src/main/java/com/example/permissionsexample/BaseActivity.java
@@ -0,0 +1,135 @@
+package com.example.permissionsexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
+
+    protected int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 20;
+    protected int MY_PERMISSION_REQUEST_WRITE_CONTACTS = 30;
+    protected int MY_PHOTO_TAGGING_PERMISSIONS = 40;
+
+    protected final String [] permissionsNeededForPhotoTagging =new String[]{Manifest.permission.CAMERA, Manifest.permission.ACCESS_COARSE_LOCATION};
+
+    String [] deniedPermissionsAmongPhotoTagging;
+
+    protected void requestRunTimePermissions(final Activity activity, final String [] permissions, final int customPermissionConstant){
+        if(permissions.length==1){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, permissions[0])){
+
+                Snackbar.make(findViewById(android.R.id.content),"App needs permission to work",Snackbar.LENGTH_INDEFINITE).setAction("ENABLE",
+                        new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                ActivityCompat.requestPermissions(activity,permissions,customPermissionConstant);
+                            }
+                        }).show();
+            }else {
+                ActivityCompat.requestPermissions(this,new String[]{permissions[0]},customPermissionConstant);
+            }
+        }else if(permissions.length > 1 && customPermissionConstant== MY_PHOTO_TAGGING_PERMISSIONS){
+            if(getDeniedPermissionsAmongPhototaggingPermissions().length==1){
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, deniedPermissionsAmongPhotoTagging[0])){
+                    Snackbar.make(findViewById(android.R.id.content),"App needs permission to work",Snackbar.LENGTH_INDEFINITE).setAction("ENABLE",
+                            new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+
+                                    ActivityCompat.requestPermissions(activity, deniedPermissionsAmongPhotoTagging,customPermissionConstant);
+                                }
+                            }).show();
+                }else {
+                    ActivityCompat.requestPermissions(activity, deniedPermissionsAmongPhotoTagging,customPermissionConstant);
+                }
+            }else if(getDeniedPermissionsAmongPhototaggingPermissions().length > 1){
+                if(isFirstTimeAskForPhotoTaggingPermission()){
+                    ActivityCompat.requestPermissions(activity, deniedPermissionsAmongPhotoTagging,customPermissionConstant);
+                }else{
+                    Snackbar.make(findViewById(android.R.id.content),"This functionality needs multiple app permissions",Snackbar.LENGTH_INDEFINITE).setAction("ENABLE",
+                            new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    ActivityCompat.requestPermissions(activity, deniedPermissionsAmongPhotoTagging,customPermissionConstant);
+                                }
+                            }).show();
+                }
+
+            }
+        }
+    }
+
+    protected boolean isFirstTimeAskForPhotoTaggingPermission(){
+        SharedPreferences sharedPreferences = getSharedPreferences("permissionasks", MODE_PRIVATE);
+        boolean isFirstTime = sharedPreferences.getBoolean("PHOTO_FIRST_PERMISSION",true);
+        if(isFirstTime){
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean("PHOTO_FIRST_PERMISSION",false);
+            editor.commit();
+        }
+        return isFirstTime;
+    }
+
+    protected boolean checkWhetherAllPermissionsPresentForPhotoTagging(){
+        for(String permission: permissionsNeededForPhotoTagging){
+            if(ActivityCompat.checkSelfPermission(this, permission)== PackageManager.PERMISSION_DENIED){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    protected String[] getDeniedPermissionsAmongPhototaggingPermissions(){
+
+        final List<String> deniedPermissions = new ArrayList<String>();
+        for(String permission: permissionsNeededForPhotoTagging){
+            if(ActivityCompat.checkSelfPermission(this,permission) == PackageManager.PERMISSION_DENIED){
+                deniedPermissions.add(permission);
+            }
+        }
+        this.deniedPermissionsAmongPhotoTagging = deniedPermissions.toArray(new String[deniedPermissions.size()]);
+        return deniedPermissionsAmongPhotoTagging;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_base, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.appPermissions: startAppPermissions();
+                                      break;
+            default: break;
+        }
+        return true;
+    }
+
+    private void startAppPermissions(){
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                Uri.fromParts("package", getPackageName(), null));
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+}
--- PermissionsExample/app/src/main/java/com/example/permissionsexample/MainActivity.java
+++ PermissionsExample/app/src/main/java/com/example/permissionsexample/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.permissionsexample;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
@@ -12,8 +13,8 @@ import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
-public class MainActivity extends AppCompatActivity {
-    private Button btnLoadContacts;
+public class MainActivity extends BaseActivity {
+
     private int MY_PERMISSION_REQUEST_READ_CONTACTS = 20;
     private int MY_PERMISSION_REQUEST_WRITE_CONTACTS = 30;
 
@@ -28,6 +29,39 @@ public class MainActivity extends AppCompatActivity {
                 loadContacts();
             }
         });
+
+        findViewById(R.id.btn_check).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if((checkWhetherAllPermissionsPresentForPhotoTagging())){
+                    Toast.makeText(MainActivity.this, R.string.permissions_granted, Toast.LENGTH_SHORT).show();
+                }else{
+                    requestRunTimePermissions(MainActivity.this, permissionsNeededForPhotoTagging, MY_PHOTO_TAGGING_PERMISSIONS);
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode == MY_PHOTO_TAGGING_PERMISSIONS){
+            if(checkWhetherAllPermissionsPresentForPhotoTagging()){
+                Toast.makeText(this, R.string.permissions_granted, Toast.LENGTH_SHORT).show();
+            }else{
+                String permissionString = getDeniedPermissionsAmongPhototaggingPermissions().length==1?"Permission":"Permissions";
+                Snackbar.make(findViewById(android.R.id.content),permissionString + " denied, photo tagging will not work. To enable now click here", Snackbar.LENGTH_INDEFINITE).setAction("ENABLE",
+                        new View.OnClickListener(){
+                            @Override
+                            public void onClick(View v){
+                                ActivityCompat.requestPermissions(MainActivity.this, getDeniedPermissionsAmongPhototaggingPermissions(), MY_PHOTO_TAGGING_PERMISSIONS);
+                            }
+                        }).show();
+            }
+        }
     }
 
     private void loadContacts() {
